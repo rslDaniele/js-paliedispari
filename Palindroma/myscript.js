2c0f8c65979486694fa122680b0e099c1044deb2
Daniele <danlup94@gmail.com>
2020-10-27 16:36:11 +0100

    Inserimento codice js

---
+++ Palindroma/myscript.js
@@ -0,0 +1,20 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// User inserts a word
+
+var parola = prompt("Inserisci una parola, vediamo se è palindroma!");
+console.log(parola);
+
+// system checks if parola is a palindrome word
+
+
+// function checkPalindroma() {
+  if (parola === parola.split('').reverse().join('')) {
+    document.write("la parola è palindroma!");
+    console.log(risposta);
+  } else {
+    document.write("la parola non è palindorma!");
+    console.log(risposta);
+  }
+// }
